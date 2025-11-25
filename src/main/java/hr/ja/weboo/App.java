128504455ab52a5e051915377225feb4c4ce042e
jdiminic <jdiminic@gmail.com>
2025-11-25 11:20:01 +0100

    Add test

--- src/main/java/hr/ja/weboo/App.java
+++ src/main/java/hr/ja/weboo/App.java
@@ -1,12 +1,15 @@
 package hr.ja.weboo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
+        log.debug("http://localhost:8080/");
         SpringApplication.run(App.class, args);
     }
 
---
+++ src/main/java/hr/ja/weboo/pages/BasePage.java
@@ -0,0 +1,12 @@
+package hr.ja.weboo.pages;
+
+import hr.ja.weboo.ui.Page;
+
+public abstract class BasePage extends Page {
+
+    protected void setDeafaults() {
+        setTitle(getTitle());
+    }
+
+
+}
